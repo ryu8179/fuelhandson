c4b3741506acdefc6660f3742319657c36cd500f
Ryugo Abe <abe.ryugo@gmail.com>
2013-07-20 15:11:53 +0900

    データの表示を行いました。

--- fuel/app/classes/controller/demo.php
+++ fuel/app/classes/controller/demo.php
@@ -1,9 +1,15 @@
 <?php
+
 class Controller_Demo extends Controller
 {
     public function action_index()
     {
         return Response::forge(View::forge('demo/index'));
     }
+    
+    public function action_showData()
+    {
+        $data = array('text' => 'Aichi');
+        return Response::forge(View::forge('demo/showdata', $data));
+    }
 }
-?>
